7a1ecd5b3f875d5bd2b8a238bd7e7df3a2087a6d
Javi Martín <javim@elretirao.net>
2020-11-23 20:51:51 +0100

    Add basic SDG Management content section

--- app/components/sdg_management/menu_component.rb
+++ app/components/sdg_management/menu_component.rb
@@ -4,4 +4,22 @@ class SDGManagement::MenuComponent < ApplicationComponent
     def sdg?
       %w[goals targets local_targets].include?(controller_name)
     end
+
+    def relatable_list_item(type, &block)
+      active = controller_name == "relations" && params[:relatable_type] == type.tableize
+
+      tag.li class: ("is-active" if active) do
+        link_to t("sdg_management.menu.#{table_name(type)}"),
+                relatable_type_path(type),
+                class: "#{table_name(type).tr("_", "-")}-link"
+      end
+    end
+
+    def relatable_type_path(type)
+      send("sdg_management_#{table_name(type)}_path")
+    end
+
+    def table_name(type)
+      type.constantize.table_name
+    end
 end
